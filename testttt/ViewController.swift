14d4d406f7ec69f4a192562dc789eaf095e7789f
suojae3 <suojae3@gmail.com>
2023-10-14 22:26:10 +0900

    Initial Commit

---
+++ testttt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testttt
+//
+//  Created by ㅣ on 2023/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
